77157f7d054f3a1c78d8ef97a00c34a6e88b99f9
jmccormac01 <jmccormac001@gmail.com>
2016-01-27 17:03:51 +0000

    initial commit of HARPS-N analysis script and git files

---
+++ AnalyseHARPS-N.py
@@ -0,0 +1,124 @@
+#
+# playing with HARPS-N spectra
+# simple script to have a fiddle with HARPS data
+#
+
+# Notes from Gzregorz Nowak on HARPS-N DRS pipeline output
+"""
+The data were reduced automatically, directly after observations by
+the DRS pipeline and the spectra were cross-correlated using G2V
+template mask. Therefore you may find in the scientific_data/
+subdirectory not only merged stellar (s1d_A) and sky (s1d_B) spectra
+(as well as spectra order by order, i.e. e2ds_A/B FITS files), but
+also FITS files with cross-correlation functions (ccf_G2_A/B), and
+bisector lines (bis_G2_A/B) and data files with Gaussian parameters
+fitted to the CCFs from different orders (_ccf_G2_A/B.tbl).
+"""
+
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as pl
+from astropy.io import fits
+from math import sqrt,ceil
+import glob as g
+import numpy as np
+import seaborn
+
+# gamma_velocity for the planet we are analysing
+gamma_vel= -6.68149
+
+# Gaussian = Aexp[ -(x-x0)^2 / (2*width^2) ]
+def Gaussian(x, *p): 
+	A, x0, width = p
+	y = np.zeros(x.shape)
+	g=A*np.exp((-(x-x0)**2)/(2*width**2))
+	return g
+
+# return estimated A, x0 and fwhm
+def estimateGaussianParams(tdata,xn):
+	return [min(t_data) - max(t_data),np.median(xn),np.std(xn)] 
+
+# code below is for analysing the stellar CCFs
+def analyseStellarSpectra(stellar_ccf_G2):
+	amp=np.empty(len(stellar_ccf_G2))
+	rv=np.empty(len(stellar_ccf_G2))
+	fwhm=np.empty(len(stellar_ccf_G2))
+	mjd=np.empty(len(stellar_ccf_G2))
+
+	for i in range(0,len(stellar_ccf_G2)):
+		h=fits.open(stellar_ccf_G2[i])
+		crval1=h[0].header['CRVAL1']
+		cdelt1=h[0].header['CDELT1']
+		mjd[i]=h[0].header['MJD-OBS']
+		data=h[0].data
+		
+		# prepare the plotting array
+		fig = pl.figure(i+1,figsize=(15,15))
+		fig.clf()
+		fig.suptitle('%s CCFs' % (stellar_ccf_G2[i]))
+		seaborn.axes_style("darkgrid")
+		montage_dim=int(ceil(sqrt(len(data))))
+		c=0
+		for j in range(0,montage_dim):
+			for k in range(0,montage_dim):
+				ax = fig.add_subplot(montage_dim, montage_dim, c+1, xticks=[], yticks=[])		
+				if c < len(data):
+					x=np.arange(0,len(data[c]))
+					xn=(x+cdelt1)+crval1
+					t_data=data[c]-max(data[c])	
+					ax.plot(xn,t_data,'b.')
+					ax.set_title('order %d' % (c+1))
+					# try fitting a Gaussian to the ccf
+					# tweak y values to help the fit
+					# only fit those that are not 0s
+					if max(data[c]) > 0 and np.std(data[c]) > 0:
+						p0=estimateGaussianParams(t_data,xn)
+						coeff,cov=curve_fit(Gaussian,xn,t_data,p0)
+						yfit = Gaussian(xn, *coeff)
+						print ('[%d/%d] x0=%.4f ' % (c+1,len(data),coeff[1]))
+						ax.plot(xn,yfit,'r--')
+						if c == len(data)-1:
+							amp[i]=coeff[0]
+							rv[i]=coeff[1]
+							fwhm[i]=coeff[2]
+					else:
+						print('[%d/%d] Cannot fit zeros' % (c+1,len(data)))
+					c+=1
+		# plot the montage of CCFs
+		fig.savefig('%s.png' % (stellar_ccf_G2[i]),dpi=300)
+
+	# plot the results from the analysis of the RVs/RM
+	mjds=mjd-int(mjd[0])
+	correction=np.average(rv)-gamma_vel
+	rv_corr=rv-correction
+	fig = pl.figure(i+1,figsize=(15,15))
+	fig.clf()
+	ax = fig.add_subplot(3,1,1)
+	ax.plot(mjds,rv_corr,'ro')
+	ax.set_ylabel('RV (km/s))')
+	ax = fig.add_subplot(3,1,2)
+	ax.plot(mjds,amp,'ro')
+	ax.set_ylabel('Inverse CCF SNR')
+	ax = fig.add_subplot(3,1,3)
+	ax.plot(mjds,fwhm,'ro')
+	ax.set_xlabel('MJD - int(MJD[0])')
+	ax.set_ylabel('FWHM (km/s))')
+	pl.savefig('SummaryResults.png',dpi=300)
+	return mjd,rv_corr,amp,fwhm
+
+
+if __name__ == '__main__':
+	# get lists of the different HARPS-N DRS
+	stellar_1d=g.glob('*_s1d_A.fits')
+	sky_1d=g.glob('*_s1d_B.fits')
+	stellar_orders=g.glob('*_e2ds_A.fits')
+	sky_orders=g.glob('*_e2ds_B.fits')
+	stellar_ccf_G2=g.glob('*ccf_G2_A.fits')
+	sky_ccf_G2=g.glob('*ccf_G2_B.fits')
+	stellar_bis_G2=g.glob('*bis_G2_A.fits')
+	sky_bis_G2=g.glob('*bis_G2_B.fits')
+	stellar_gauss_ccf_G2=g.glob('*ccf_G2_A.tbl')
+	sky_gauss_ccf_G2=g.glob('*ccf_G2_B.tbl')
+
+	# analyse the stellar spectra
+	mjd,rv_corr,amp,fwhm=analyseStellarSpectra(stellar_ccf_G2)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+## HARPS-N Analysis
+Some toy scripts to fiddle with HARPS-N spectra and do some simple CCF fits
