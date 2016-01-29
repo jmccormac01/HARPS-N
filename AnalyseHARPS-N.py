ca0d8684a2754cc0ea9813a71e9d1141051582db
jmccormac01 <jmccormac001@gmail.com>
2016-01-29 10:56:47 +0000

    combined the stellar and sky CCF functions into one

--- AnalyseHARPS-N.py
+++ AnalyseHARPS-N.py
@@ -42,18 +42,18 @@ def Gaussian(x, *p):
 	return g
 
 # return estimated A, x0 and fwhm
-def estimateGaussianParams(tdata,xn):
+def estimateGaussianParams(t_data,xn):
 	return [min(t_data) - max(t_data),np.median(xn),np.std(xn)] 
 
-# code below is for analysing the stellar CCFs
-def analyseStellarCCFs(stellar_ccf_G2):
-	amp=np.empty(len(stellar_ccf_G2))
-	rv=np.empty(len(stellar_ccf_G2))
-	fwhm=np.empty(len(stellar_ccf_G2))
-	mjd=np.empty(len(stellar_ccf_G2))
-
-	for i in range(0,len(stellar_ccf_G2)):
-		h=fits.open(stellar_ccf_G2[i])
+# code below is for analysing sky or star CCFs
+def analyseCCFs(ccf_G2):
+	amp=np.empty(len(ccf_G2))
+	rv=np.empty(len(ccf_G2))
+	fwhm=np.empty(len(ccf_G2))
+	mjd=np.empty(len(ccf_G2))
+	combined_ccfs=[]
+	for i in range(0,len(ccf_G2)):
+		h=fits.open(ccf_G2[i])
 		crval1=h[0].header['CRVAL1']
 		cdelt1=h[0].header['CDELT1']
 		mjd[i]=h[0].header['MJD-OBS']
@@ -62,95 +62,7 @@ def analyseStellarCCFs(stellar_ccf_G2):
 		# prepare the plotting array
 		fig = pl.figure(i+1,figsize=(15,15))
 		fig.clf()
-		fig.suptitle('%s CCFs' % (stellar_ccf_G2[i]))
-		seaborn.axes_style("darkgrid")
-		montage_dim=int(ceil(sqrt(len(data))))
-		c=0
-		for j in range(0,montage_dim):
-			for k in range(0,montage_dim):
-				ax = fig.add_subplot(montage_dim, montage_dim, c+1, xticks=[], yticks=[])		
-				if c < len(data):
-					# try fitting a Gaussian to the ccf
-					# tweak y values to help the fit
-					# only fit those that are not 0s
-					x=np.arange(0,len(data[c]))
-					xn=(x+cdelt1)+crval1
-					t_data=data[c]-max(data[c])	
-					ax.plot(xn,t_data,'b.')
-					ax.set_title('order %d' % (c+1))
-					if max(data[c]) > 0 and np.std(data[c]) > 0:
-						p0=estimateGaussianParams(t_data,xn)
-						coeff,cov=curve_fit(Gaussian,xn,t_data,p0)
-						yfit = Gaussian(xn, *coeff)
-						print ('[%d/%d] x0=%.4f ' % (c+1,len(data),coeff[1]))
-						ax.plot(xn,yfit,'r--')
-						if c == len(data)-1:
-							amp[i]=coeff[0]
-							rv[i]=coeff[1]
-							fwhm[i]=coeff[2]
-					else:
-						print('[%d/%d] Cannot fit zeros' % (c+1,len(data)))
-					c+=1
-		# plot the montage of CCFs
-		fig.savefig('%s.png' % (stellar_ccf_G2[i]),dpi=300)
-
-	# plot the results from the analysis of the RVs/RM
-	mjds=mjd-int(mjd[0])
-	correction=np.average(rv)-gamma_vel
-	rv_corr=rv-correction
-	fig = pl.figure(i+1,figsize=(15,15))
-	fig.clf()
-	ax = fig.add_subplot(3,1,1)
-	ax.plot(mjds,rv_corr,'ro')
-	ax.set_ylabel('RV (km/s))')
-	ax = fig.add_subplot(3,1,2)
-	ax.plot(mjds,amp,'ro')
-	ax.set_ylabel('Inverse CCF SNR')
-	ax = fig.add_subplot(3,1,3)
-	ax.plot(mjds,fwhm,'ro')
-	ax.set_xlabel('MJD - int(MJD[0])')
-	ax.set_ylabel('FWHM (km/s))')
-	pl.savefig('SummaryResults.png',dpi=300)
-	return mjd,rv_corr,amp,fwhm
-
-# code below is for analysing the sky CCFs
-#def analyseSkyCCFs(sky_ccf_G2):
-
-
-
-if __name__ == '__main__':
-	# get lists of the different HARPS-N DRS
-	stellar_1d=g.glob('*_s1d_A.fits')
-	sky_1d=g.glob('*_s1d_B.fits')
-	stellar_orders=g.glob('*_e2ds_A.fits')
-	sky_orders=g.glob('*_e2ds_B.fits')
-	stellar_ccf_G2=g.glob('*ccf_G2_A.fits')
-	sky_ccf_G2=g.glob('*ccf_G2_B.fits')
-	stellar_bis_G2=g.glob('*bis_G2_A.fits')
-	sky_bis_G2=g.glob('*bis_G2_B.fits')
-	stellar_gauss_ccf_G2=g.glob('*ccf_G2_A.tbl')
-	sky_gauss_ccf_G2=g.glob('*ccf_G2_B.tbl')
-
-	# analyse the stellar spectra
-	#mjd,rv_corr,amp,fwhm=analyseStellarCCFs(stellar_ccf_G2)
-	#analyseSkyCCFs(sky_ccf_G2)
-	
-	amp=np.empty(len(sky_ccf_G2))
-	rv=np.empty(len(sky_ccf_G2))
-	fwhm=np.empty(len(sky_ccf_G2))
-	mjd=np.empty(len(sky_ccf_G2))
-
-	for i in range(0,len(sky_ccf_G2)):
-		h=fits.open(sky_ccf_G2[i])
-		crval1=h[0].header['CRVAL1']
-		cdelt1=h[0].header['CDELT1']
-		mjd[i]=h[0].header['MJD-OBS']
-		data=h[0].data
-		
-		# prepare the plotting array
-		fig = pl.figure(i+1,figsize=(15,15))
-		fig.clf()
-		fig.suptitle('%s CCFs' % (sky_ccf_G2[i]))
+		fig.suptitle('%s CCFs' % (ccf_G2[i]))
 		seaborn.axes_style("darkgrid")
 		montage_dim=int(ceil(sqrt(len(data))))
 		c=0
@@ -177,15 +89,107 @@ if __name__ == '__main__':
 								amp[i]=coeff[0]
 								rv[i]=coeff[1]
 								fwhm[i]=coeff[2]
+								combined_ccfs.append(data[c])
 						except RuntimeError:
 							print ('[%d/%d] fit did not converge ' % (c+1,len(data)))
 							if c == len(data)-1:
 								print ('[%d/%d] FINAL FIT DID NOT CONVERGE! ' % (c+1,len(data)))
-
+								combined_ccfs.append(data[c])
 					else:
 						print('[%d/%d] Cannot fit zeros' % (c+1,len(data)))
 					c+=1
 					#print("[%d/%d]" % (c,len(data)))
 		# plot the montage of CCFs
-		fig.savefig('%s.png' % (sky_ccf_G2[i]),dpi=300)
+		fig.savefig('%s.png' % (ccf_G2[i]),dpi=300)
+	return mjd,rv_corr,amp,fwhm,combined_ccfs
+
+# make a plot of the RV results
+def makeSummaryPlot(mjd,rv,amp,fwhm):
+	# plot the results from the analysis of the RVs/RM
+	mjds=mjd-int(mjd[0])
+	fig = pl.figure(i+1,figsize=(15,15))
+	fig.clf()
+	ax = fig.add_subplot(3,1,1)
+	ax.plot(mjds,rv,'ro')
+	ax.set_ylabel('RV (km/s))')
+	ax = fig.add_subplot(3,1,2)
+	ax.plot(mjds,amp,'ro')
+	ax.set_ylabel('Inverse CCF SNR')
+	ax = fig.add_subplot(3,1,3)
+	ax.plot(mjds,fwhm,'ro')
+	ax.set_xlabel('MJD - int(MJD[0])')
+	ax.set_ylabel('FWHM (km/s))')
+	pl.savefig('SummaryResults.png',dpi=300)
+
+if __name__ == '__main__':
+	# get lists of the different HARPS-N DRS
+	stellar_1d=g.glob('*_s1d_A.fits')
+	sky_1d=g.glob('*_s1d_B.fits')
+	stellar_orders=g.glob('*_e2ds_A.fits')
+	sky_orders=g.glob('*_e2ds_B.fits')
+	stellar_ccf_G2=g.glob('*ccf_G2_A.fits')
+	sky_ccf_G2=g.glob('*ccf_G2_B.fits')
+	stellar_bis_G2=g.glob('*bis_G2_A.fits')
+	sky_bis_G2=g.glob('*bis_G2_B.fits')
+	stellar_gauss_ccf_G2=g.glob('*ccf_G2_A.tbl')
+	sky_gauss_ccf_G2=g.glob('*ccf_G2_B.tbl')
+
+	# analyse the stellar spectra
+	mjd_star,rv_star,amp_star,fwhm_star,ccfs_star=analyseCCFs(stellar_ccf_G2)
+	mjd_sky,rv_sky,amp_sky,fwhm_sky,ccfs_sky=analyseCCFs(sky_ccf_G2)
+
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
 
